d0cf6d69581e9ed9c3f9b71332b0b3394ea48a0e
leslie628 <lf24aap@herts.ac.uk>
2025-06-26 18:35:10 +0100

    added jwt token changes for auth login micro app to simulate login functionality (out of scope for this project) and auth micro app import map configuration

--- src/test-root-config.js
+++ src/test-root-config.js
@@ -22,4 +22,58 @@ const layoutEngine = constructLayoutEngine({ routes, applications });
 
 applications.forEach(registerApplication);
 layoutEngine.activate();
+redirectToAuthIfNotLoggedIn();
+redirectAfterLogin();
 start();
+
+function toggleSidebar() {
+  const token = localStorage.getItem('token');
+  const sidebar = document.getElementById('sidebar');
+  if (!sidebar) return;
+ sidebar.style.display = token ? 'block' : 'none';
+}
+
+toggleSidebar();
+
+// Hide/ display side bar based on token -> if login is successfully then only show side bar and micro apps else redirect to auth login micro app
+window.addEventListener('storage', (event) => { //listen browser application storage change
+  if (event.key === 'token') {
+    toggleSidebar();
+    redirectToAuthIfNotLoggedIn();
+    redirectAfterLogin();
+  }
+});
+
+window.addEventListener('auth-success', () => {
+  toggleSidebarVisibility();
+  const currentPath = window.location.pathname;
+  if (currentPath === '/auth') {
+    window.history.replaceState(null, '', '/settings');
+    // notify single spa router to load settings micro app
+    window.dispatchEvent(new PopStateEvent('popstate'));
+  }
+});
+
+//  redirect to login micro app if not logged in yet
+function redirectToAuthIfNotLoggedIn() {
+  const token = localStorage.getItem('token');
+  const currentPath = window.location.pathname;
+
+  if (!token && currentPath !== '/auth') {
+    window.history.pushState(null, '', '/auth');
+    // notify single spa router to load auth micro app
+    window.dispatchEvent(new PopStateEvent('popstate'));
+  }
+}
+
+// After user login successfull redirect to settings page
+function redirectAfterLogin() {
+  const token = localStorage.getItem('token');
+  const currentPath = window.location.pathname;
+
+  if (token && currentPath === '/auth') {
+    window.history.replaceState(null, '', '/settings');
+     // notify single spa router to load settings micro app
+    window.dispatchEvent(new PopStateEvent('popstate'));
+  }
+}
